5ef44da9ef1c72eb44b166049c599a72d25aeba2
Veronica Lopez <nicaa0695@gmail.com>
2019-07-24 01:19:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name=name 
+    @roster = {}
+  end
+  def add_student(name, grade)
+    roster[grade] = [] unless roster[grade]
+    roster[grade] << name
+  end
+  def grade(num)
+    roster[num]
+  end
+  def sort 
+    sorted = {}
+end
